db8bb9652ead52b4eb61b562aa55737210267037
configMaster <149285669+PricklyPear-who-codes@users.noreply.github.com>
2024-05-14 08:40:48 -0400

    made DogeV4 have a full screen property by double clicking the nav bar

--- emailCompany.js
+++ emailCompany.js
@@ -6,9 +6,9 @@ function email() {
         if (body == null || body == "") {
             alert("No Body");
         } else {
-            window.open(`mailto:Tomlinson@bbn-tenexa?subject=${subject}&body=${body}`);
+            window.open(`mailto:vividdisco12@gmail.com?subject=${subject}&body=${body}`);
         }
     } else {
-        alert("No Subject");
+        alert("Cancelled");
     }
 }
