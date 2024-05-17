8886a8f3059b049b927d039a3d0e9e508879369d
configMaster <149285669+PricklyPear-who-codes@users.noreply.github.com>
2024-05-17 12:14:20 -0400

    delete for update

--- showNotification.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", () => {
-    document.getElementById("noti").style.bottom = "15px";
-    setTimeout(() => {
-        document.getElementById("noti").style.bottom = "-60px";
-    }, 3000);
-});
--- viewElements.js
+++
@@ -1,3 +0,0 @@
-function viewElement(element) {
-    document.getElementById(element).scrollIntoView();
-}
