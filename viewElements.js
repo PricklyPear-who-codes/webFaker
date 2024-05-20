66e82fcca48dbff50e9cebfe6eec67d5d32d02a6
configMaster <149285669+PricklyPear-who-codes@users.noreply.github.com>
2024-05-20 19:13:54 -0400

    updates

--- viewElements.js
+++
@@ -1,7 +0,0 @@
-function viewElement(element) {
-    document.getElementById(element).scrollIntoView();
-}
-
-window.addEventListener("dblclick", () => {
-    console.log(document.getElementById("top-nav").style.width);
-});
